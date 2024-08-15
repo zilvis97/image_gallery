58bddcda48b37e4b4cb557e410ad47bf005b8a88
zilvis97 <zilvinas.skuodys@gmail.com>
2024-08-15 23:38:27 +0300

    🔥 Remove unused code

--- lib/widgets/image_item.dart
+++ lib/widgets/image_item.dart
@@ -1,20 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:image_gallery/models/image_data.dart';
 
 class ImageItem extends StatelessWidget {
-  ImageItem(this.image, {super.key});
+  const ImageItem(this.image, {super.key});
 
   final ImageData image;
-
-  final customCacheManager = CacheManager(
-    Config(
-      'cacheKey',
-      stalePeriod: const Duration(days: 3),
-      maxNrOfCacheObjects: 100,
-    ),
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +15,6 @@ class ImageItem extends StatelessWidget {
         children: [
           Text(image.id),
           CachedNetworkImage(
-            cacheManager: customCacheManager,
             imageUrl: image.downloadUrl,
             placeholder: (_, __) => const Icon(Icons.photo_outlined),
             errorWidget: (context, url, error) => const Icon(Icons.error),
